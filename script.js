c72b5dd8a5ab1cd972fd3a9630c44fa35df5eee2
Solademi Opeyemi Simon <kaydroit247@gmail.com>
2024-12-13 02:08:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -43,7 +43,40 @@ function validateForm() {
 
     // Submit the form if valid
     if (isValid) {
-        alert('Form submitted successfully!');
-        document.getElementById('feedbackForm').submit();
+        db.collection("feedbacks").add({
+            name: name,
+            email: email,
+            feedback: feedback,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(() => {
+            alert("Feedback submitted successfully!");
+            document.getElementById("feedbackForm").reset();
+        }).catch((error) => {
+            console.error("Error submitting feedback: ", error);
+            alert("An error occurred. Please try again.");
+        });
     }
+
 }
+  // Firebase configuration
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC_Vf6FlHWQR8Yzo3Qt0opwjuNXE9J_qbQ",
+    authDomain: "group-2-7dea4.firebaseapp.com",
+    projectId: "group-2-7dea4",
+    storageBucket: "group-2-7dea4.firebasestorage.app",
+    messagingSenderId: "494990664818",
+    appId: "1:494990664818:web:17f12aff4ef22981ef8eeb",
+    measurementId: "G-6D5FL059CY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
